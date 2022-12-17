48dfa2d2960a93e4996dafdd2c80d4aa1cfe0732
Abhijit Deb <imabhijitdeb@gmail.com>
2022-12-17 20:58:19 +0530

    Small change

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='max-w-screen-lg md:mt-0 mt-[140px]  p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
 
-            <p className='text-xl mt-20'>
+            <p className='text-xl sm:mt-20 mt-10'>
             I'm pursuing B.E from Sri Sairam College of Engineering, Bangalore, India with specialization in Computer Engineering. I am a technology enthusiast and have been learning different programming languages and have handson experience specially in Python. I have done projects in Machine Learning & Artificial Intelligence using TensorFlow, Keras, OpenCV,etc.
             </p>
 
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4  md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-3xl sm:text-7xl font-bold text-white'>I'm a python developer and frontend developer.</h2>
+                <h2 className='text-3xl sm:text-7xl sm:m-1 m-[90px] font-bold text-white'>I'm a python developer and frontend developer.</h2>
                 <p className='text-gray-500 py-4 max-w-md text-2xl'>Hi, I'm Abhijit Deb an engineering grad from Bangalore, India. I love coding and developing application using the latest technologies. Basically an intern working as a frontend developer and python application developer.</p>
 
                 <div>
